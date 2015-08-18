196efd6f7452442db2a39fa9373b7547b75631ac
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-08-18 19:27:44 +0100

    Decode what he's doing in prime.wsa

--- gen-prime.c
+++ gen-prime.c
@@ -9,102 +9,113 @@ ws_pushs("prim numbers [2-100]");
 ws_call(atom("printsln"));
 
 
+{
+    // for(i=0; i<100; i++) memory[100+i] = 1;
 
-ws_push(0);
-//for loop
-ws_label(atom("f_start_1"));
-ws_dup(); ws_push(100); ws_sub();
-ws_jz(atom("f_finished_1"));
+    ws_push(0);
+    //for loop
+    ws_label(atom("f_start_1"));
+    ws_dup(); ws_push(100); ws_sub();
+    ws_jz(atom("f_finished_1"));
 
-ws_dup();
-ws_push(100); ws_add();
-        ws_push(1);
-ws_store();
+    ws_dup();
+    ws_push(100); ws_add();
+	    ws_push(1);
+    ws_store();
 
-ws_push(1); ws_add();
-ws_jump(atom("f_start_1"));
+    ws_push(1); ws_add();
+    ws_jump(atom("f_start_1"));
 
-ws_label(atom("f_finished_1"));
-ws_drop();
+    ws_label(atom("f_finished_1"));
+    ws_drop();
+}
 
 //debug_printstack
 //debug_printheap
 
-ws_push(2);
-//for loop
-ws_label(atom("f_start_2"));
-ws_dup(); ws_push(11); ws_sub();
-ws_jz(atom("f_finished_2"));
+{
+/*
+    for(i=2; i<11; i++)
+	for(j=2; j<100; j++)
+	    memory[100+i*j] = 0;
+ */
+
+    ws_push(2);
+    //for loop
+    ws_label(atom("f_start_2"));
+    ws_dup(); ws_push(11); ws_sub();
+    ws_jz(atom("f_finished_2"));
 
 
 
-        ws_push(2);
-        //for loop
-        ws_label(atom("f_start_3"));
-ws_dup(); ws_push(100); ws_sub();
-        ws_jz(atom("f_finished_3"));
+	    ws_push(2);
+	    //for loop
+	    ws_label(atom("f_start_3"));
+    ws_dup(); ws_push(100); ws_sub();
+	    ws_jz(atom("f_finished_3"));
 
-ws_dup();
-                        ws_push(1);
-ws_swap();
-ws_store();
+    ws_dup();
+			    ws_push(1);
+    ws_swap();
+    ws_store();
 
-ws_swap();
+    ws_swap();
 
-ws_dup();
-                        ws_push(2);
-ws_swap();
-ws_store();
+    ws_dup();
+			    ws_push(2);
+    ws_swap();
+    ws_store();
 
-ws_swap();
+    ws_swap();
 
-ws_push(1); ws_fetch();
-ws_push(2); ws_fetch();
-ws_mul();
-ws_push(100); ws_add();
+    ws_push(1); ws_fetch();
+    ws_push(2); ws_fetch();
+    ws_mul();
+    ws_push(100); ws_add();
 
-                ws_push(0);
-ws_store();
+		    ws_push(0);
+    ws_store();
 
-ws_push(1); ws_add();
+    ws_push(1); ws_add();
 
-        ws_jump(atom("f_start_3"));
-        ws_label(atom("f_finished_3"));
-        ws_drop();
-
-
-ws_push(1); ws_add();
-
-ws_jump(atom("f_start_2"));
-ws_label(atom("f_finished_2"));
-ws_drop();
+	    ws_jump(atom("f_start_3"));
+	    ws_label(atom("f_finished_3"));
+	    ws_drop();
 
 
+    ws_push(1); ws_add();
 
-ws_push(2);
-//for loop
-ws_label(atom("f_start_4"));
-ws_dup(); ws_push(100); ws_sub();
-ws_jz(atom("f_finished_4"));
+    ws_jump(atom("f_start_2"));
+    ws_label(atom("f_finished_2"));
+    ws_drop();
+}
 
-ws_dup();
-ws_push(100); ws_add();
-ws_fetch();
-ws_jz(atom("nodraw"));
+{
+    ws_push(2);
+    //for loop
+    ws_label(atom("f_start_4"));
+    ws_dup(); ws_push(100); ws_sub();
+    ws_jz(atom("f_finished_4"));
 
-ws_dup();
-ws_outn();
+    ws_dup();
+    ws_push(100); ws_add();
+    ws_fetch();
+    ws_jz(atom("nodraw"));
 
-ws_pushs(" ");
-ws_call(atom("prints"));
+    ws_dup();
+    ws_outn();
 
-ws_label(atom("nodraw"));
+    ws_pushs(" ");
+    ws_call(atom("prints"));
 
-ws_push(1); ws_add();
-ws_jump(atom("f_start_4"));
+    ws_label(atom("nodraw"));
 
-ws_label(atom("f_finished_4"));
-ws_drop();
+    ws_push(1); ws_add();
+    ws_jump(atom("f_start_4"));
+
+    ws_label(atom("f_finished_4"));
+    ws_drop();
+}
 
 ws_push(0);
 ws_call(atom("printsln"));
