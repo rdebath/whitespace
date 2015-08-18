86be94998075c2b27cda11b3874e3f7a0636821d
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-08-18 20:53:46 +0100

    Complete wsa.sed and gencode.h
    Now runs the Whitespace interpreter in Whitespace.

--- ws_gencode.h
+++ ws_gencode.h
@@ -9,6 +9,7 @@
 
 static void putnum(unsigned long num, int clipped);
 static void putsnum(long val);
+static int atom(char * s);
 
 static void ws_push(int v) { printf("  "); putsnum(v); }
 static void ws_label(int v) { printf("\n  "); putnum(v, BIJECTIVE); }
@@ -16,27 +17,83 @@ static void ws_jump(int v) { printf("\n \n"); putnum(v, BIJECTIVE); }
 static void ws_jz(int v) { printf("\n\t "); putnum(v, BIJECTIVE); }
 static void ws_jn(int v) { printf("\n\t\t"); putnum(v, BIJECTIVE); }
 static void ws_call(int v) { printf("\n \t"); putnum(v, BIJECTIVE); }
+static void ws_exit() { printf("\n\n\n"); }
+static void ws_return() { printf("\n\t\n"); }
 
 static void ws_dup() { printf(" \n "); }
 static void ws_swap() { printf(" \n\t"); }
-static void ws_exit() { printf("\n\n\n"); }
+static void ws_drop() { printf(" \n\n"); }
 
 static void ws_outc() { printf("\t\n  "); }
 static void ws_readc() { printf("\t\n\t "); }
+static void ws_outn() { printf("\t\n \t"); }
+static void ws_readn() { printf("\t\n\t\t"); }
 
 static void ws_add() { printf("\t   "); }
-static void ws_mul() { printf("\t  \n"); }
 static void ws_sub() { printf("\t  \t"); }
+static void ws_mul() { printf("\t  \n"); }
+static void ws_div() { printf("\t \t "); }
+static void ws_mod() { printf("\t \t\t"); }
 
-static void ws_drop() { printf(" \n\n"); }
 static void ws_fetch() { printf("\t\t\t"); }
 static void ws_store() { printf("\t\t "); }
 
-static void ws_outn() { printf("\t\n \t"); }
 #define ws_retrieve ws_fetch
 
-static void ws_return() { printf("\n\t\n"); }
+/* Version 0.3 functions */
+static void ws_pick(int v) { printf(" \t "); putsnum(v); }
+static void ws_slide(int v) { printf(" \t\n"); putsnum(v); }
 
+/* Minor macros */
+
+/* Push a string onto the stack in reverse order. */
+static void ws_pushs(char * s)
+{
+    int c=0, i = strlen(s) +1;
+    for(; i>0 ; i--) {
+	if (c!=0 && c == s[i-1])
+	    ws_dup();
+	else
+	    ws_push(c = s[i-1]);
+    }
+}
+
+/* Jump if positive */
+static void ws_jp(int v) {
+    int t = atom("");
+    ws_dup();
+    ws_jn(t);
+    ws_dup();
+    ws_jz(t);
+    ws_drop();
+    ws_jump(v);
+    ws_label(t);
+    ws_drop();
+}
+
+/* Jump if positive or zero */
+static void ws_jpz(int v) {
+    int t = atom("");
+    ws_jn(t);
+    ws_jump(v);
+    ws_label(t);
+}
+
+/* Jump if negative or zero */
+static void ws_jnz(int v) {
+    int t1 = atom("");
+    int t2 = atom("");
+    ws_dup();
+    ws_jn(t1);
+    ws_dup();
+    ws_jz(t1);
+    ws_jump(t2);
+    ws_label(t1);
+    ws_drop();
+    ws_jump(v);
+    ws_label(t2);
+    ws_drop();
+}
 
 static void
 putnum(unsigned long num, int clipped)
@@ -78,19 +135,10 @@ putsnum(long val)
     }
 }
 
-static void ws_pushs(char * s)
-{
-    int c=0, i = strlen(s) +1;
-    for(; i>0 ; i--) {
-	if (c!=0 && c == s[i-1])
-	    ws_dup();
-	else
-	    ws_push(c = s[i-1]);
-    }
-}
-
 /*
  * This is VERY slow, if you have more than a few dozen symbols RELACE THIS.
+ *
+ * Note: an empty string gives a unique id.
  */
 static int atom(char * s)
 {
@@ -102,10 +150,11 @@ static int atom(char * s)
     if (curcount >= maxcount) {
 	stringlist = realloc(stringlist, (maxcount+=1024)*sizeof*stringlist);
     }
-    for(i=0; i<curcount; i++) {
+    if(*s) for(i=0; i<curcount; i++) {
 	if (strcmp(s, stringlist[i]) == 0)
 	    return i+1;
     }
     stringlist[curcount++] = s;
+    // fprintf(stderr, "Label %d = \"%s\"\n", curcount, s);
     return curcount;
 }
