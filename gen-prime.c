b4d0c07803ad4dae2bf098752c268141638f2dac
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-08-19 11:42:03 +0100

    Use automatic atoms on prime.

--- gen-prime.c
+++ gen-prime.c
@@ -1,4 +1,7 @@
 
+#define ATOMISE_LABELS
+#define BIJECTIVE 1
+
 #include "ws_gencode.h"
 
 main()
@@ -6,7 +9,7 @@ main()
 // get prim numbers
 
 ws_pushs("prim numbers [2-100]");
-ws_call(atom("printsln"));
+ws_call(printsln);
 
 
 {
@@ -14,9 +17,9 @@ ws_call(atom("printsln"));
 
     ws_push(0);
     //for loop
-    ws_label(atom("f_start_1"));
+    ws_label(f_start_1);
     ws_dup(); ws_push(100); ws_sub();
-    ws_jz(atom("f_finished_1"));
+    ws_jz(f_finished_1);
 
     ws_dup();
     ws_push(100); ws_add();
@@ -24,9 +27,9 @@ ws_call(atom("printsln"));
     ws_store();
 
     ws_push(1); ws_add();
-    ws_jump(atom("f_start_1"));
+    ws_jump(f_start_1);
 
-    ws_label(atom("f_finished_1"));
+    ws_label(f_finished_1);
     ws_drop();
 }
 
@@ -42,17 +45,17 @@ ws_call(atom("printsln"));
 
     ws_push(2);
     //for loop
-    ws_label(atom("f_start_2"));
+    ws_label(f_start_2);
     ws_dup(); ws_push(11); ws_sub();
-    ws_jz(atom("f_finished_2"));
+    ws_jz(f_finished_2);
 
 
 
 	    ws_push(2);
 	    //for loop
-	    ws_label(atom("f_start_3"));
+	    ws_label(f_start_3);
     ws_dup(); ws_push(100); ws_sub();
-	    ws_jz(atom("f_finished_3"));
+	    ws_jz(f_finished_3);
 
     ws_dup();
 			    ws_push(1);
@@ -78,63 +81,63 @@ ws_call(atom("printsln"));
 
     ws_push(1); ws_add();
 
-	    ws_jump(atom("f_start_3"));
-	    ws_label(atom("f_finished_3"));
+	    ws_jump(f_start_3);
+	    ws_label(f_finished_3);
 	    ws_drop();
 
 
     ws_push(1); ws_add();
 
-    ws_jump(atom("f_start_2"));
-    ws_label(atom("f_finished_2"));
+    ws_jump(f_start_2);
+    ws_label(f_finished_2);
     ws_drop();
 }
 
 {
     ws_push(2);
     //for loop
-    ws_label(atom("f_start_4"));
+    ws_label(f_start_4);
     ws_dup(); ws_push(100); ws_sub();
-    ws_jz(atom("f_finished_4"));
+    ws_jz(f_finished_4);
 
     ws_dup();
     ws_push(100); ws_add();
     ws_fetch();
-    ws_jz(atom("nodraw"));
+    ws_jz(nodraw);
 
     ws_dup();
     ws_outn();
 
     ws_pushs(" ");
-    ws_call(atom("prints"));
+    ws_call(prints);
 
-    ws_label(atom("nodraw"));
+    ws_label(nodraw);
 
     ws_push(1); ws_add();
-    ws_jump(atom("f_start_4"));
+    ws_jump(f_start_4);
 
-    ws_label(atom("f_finished_4"));
+    ws_label(f_finished_4);
     ws_drop();
 }
 
 ws_push(0);
-ws_call(atom("printsln"));
+ws_call(printsln);
 
 ws_exit();
 
-ws_label(atom("prints"));
+ws_label(prints);
 
 ws_dup();
-ws_jz(atom("prints_end"));
+ws_jz(prints_end);
 ws_outc();
-ws_jump(atom("prints"));
+ws_jump(prints);
 
-ws_label(atom("prints_end"));
+ws_label(prints_end);
 ws_drop();
 ws_return();
 
-ws_label(atom("printsln"));
-ws_call(atom("prints"));
+ws_label(printsln);
+ws_call(prints);
 ws_push(10);
 ws_outc();
 ws_return();
