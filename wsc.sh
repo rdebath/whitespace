8e6af6a89de54cb44741ef58aaefbf3d0998163b
Robert de Bath <rdebath@tvisiontech.co.uk>
2015-08-20 19:36:47 +0100

    Cleanup some old files.
    The 'mkt' script is done by make.
    The awk script is now completely obsolete as ws2c.c can do it all.
    The wsc.sh is almost obsolete, it's now mostly documentation.

--- wsc.sh
+++ wsc.sh
@@ -2,7 +2,7 @@
 
 {
     # awk -f wsc.awk "$1" > /tmp/_wsc.c &&
-    ./ws2c "$1" > /tmp/_wsc.c &&
+    ./ws2c -c "$1" > /tmp/_wsc.c &&
     gcc -c -O3 $2 -o /tmp/_wsc.o /tmp/_wsc.c &&
     gcc    -O3 $3 -o /tmp/_wsc /tmp/_wsc.o
 } || exit
